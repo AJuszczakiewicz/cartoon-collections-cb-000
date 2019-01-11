98b9e8bb8153b165e0c2aa15e09ed68c2de907b5
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-01-11 15:40:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves_array)# code an argument here
 end
 
 def summon_captain_planet(planeteers_array)# code an argument here
-  planeteers_array.map{|planeteer| planeteer.capitalize }
+  planeteers_array.map{|planeteer| planeteer.capitalize << "!" }
 end
 
 def long_planeteer_calls# code an argument here
